984ac8e60a63d918b0fbb7dd10b95ab55f1f1f8c
USER10 <samuelhtosusa@gmail.com>
2021-03-25 19:41:58 -0300

    Aula 25-03

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # criando dicionarios
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    estados_capitais = {'Maranhão': 'São Luiz',
+                        'Mato Grosso': 'Cuiaba',
+                        'Mato Grosso do Sul': 'Campo Grande',
+                        'Minas Gerais': 'Belo Horizonte',
+                        }
+
+print(estados_capitais)
+
+'''dict()#construtor
+    1)

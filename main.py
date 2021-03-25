662e17ff328382046f03826a9c15520913f19bd3
USER10 <samuelhtosusa@gmail.com>
2021-03-25 19:55:57 -0300

    Aula 25-03

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ if __name__ == '__main__':
 
 print(estados_capitais)
 
+# as chaves nao são strings
+dict_01 = dict(mg='BH', MT='CB', MS='CG')
+print(f'dict_01 {dict_01}')
+
+# as chaves valor são tuplas
+dict_02 = dict([('MG', 'BH'), ('MT', 'CB'), ('MS', 'CG')])
+print(f'dict_02 {dict_02}')
+
+# par chaves são listas
+dict_03 = dict((['MG', 'BH'], ['MT', 'CB'], ['MS', 'CG']))
+print(f'dict_02 {dict_03}')
+
+print(f'dict_03 [MG] = {dict_03["MG"]}')
+
 '''dict()#construtor
-    1)
+    1)dict (**kwargs) -> seq K : valve
+    2)dict (mapping, **kwargs)'''
